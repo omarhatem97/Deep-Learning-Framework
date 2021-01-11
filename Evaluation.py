0132c841dbe6476d44bf0a5dbfe2717575f2a0d0
omarhatem97 <omarhatem221@gmail.com>
2021-01-11 02:26:16 +0200

    Implement all function in Evaluation.py except confusion_matrix

--- Evaluation.py
+++ Evaluation.py
@@ -3,32 +3,41 @@
 def confusion_matrix (label, predicted_value):
     """ takes label, predicted_value as vectors
         returns confusion matrix , tp, fp, tn, fn """
-    return None
+
 
 
 def accuracy (label, predicted_value):
     """takes label , predicted_value as vectors
         return accuracy """
 
-    return None
+    _, tp, fp, tn, fn = confusion_matrix(label, predicted_value)
+    Accuracy = (tp+tn) / (tp+fp+tn+fn)
+    return Accuracy
 
 
 def precision (label, predicted_value):
     """takes label , predicted_value as vectors
         return precision """
 
-    return None
+    _, tp, fp, tn, fn = confusion_matrix(label, predicted_value)
+    Precision = tp / tp + fp
+    return Precision
 
 
 def recall (label, predicted_value):
     """takes label , predicted_value as vectors
         return recall """
 
-    return None
+    _, tp, fp, tn, fn = confusion_matrix(label, predicted_value)
+    Recall = tp / (tp + fn)
+    return Recall
 
 
 def F1_score(label, predicted_value):
     """takes label , predicted_value as vectors
         return F1_score """
 
-    return None
+    Precision = precision(label, predicted_value)
+    Recall = recall(label, predicted_value)
+    F1 = 2 * (Precision * Recall)/(Precision + Recall)
+    return F1
