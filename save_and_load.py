d25825b7772c0df5c26f49f00d436578bd5181fa
omarhatem97 <omarhatem221@gmail.com>
2021-01-11 02:07:41 +0200

    Modify save_model , load_model functions

--- save_and_load.py
+++ save_and_load.py
@@ -1,9 +1,17 @@
+import pickle
+
 def save_model(model, filename):
     """saves model into a file named 'filename' """
-    return None
+
+    pickle_out = open("model.pickle", "wb")
+    pickle.dump(model, pickle_out)
+    pickle_out.close()
 
 
 def load_mode(filename):
     """loads a model from a file and returns model"""
 
-    return None
+    pickle_in = open(filename, "rb")
+    model = pickle.load(pickle_in)
+    return model
+
