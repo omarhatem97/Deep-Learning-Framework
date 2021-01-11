e596af7c4abfe5b54bbb30647b73ad9113bec765
omarhatem97 <omarhatem221@gmail.com>
2021-01-11 02:08:33 +0200

    Implement visualize function

--- visualization.py
+++ visualization.py
@@ -1,5 +1,18 @@
-def visualize (loss, epochs):
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualize (loss, epochs, learning_rate):
     """
     draw graph between number of epochs on x-axis and losses on y-axis
     """
-    return None
+
+    plt.plot(np.squeeze(loss))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+
+
+
+
