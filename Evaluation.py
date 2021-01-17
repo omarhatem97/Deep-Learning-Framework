8f4ddf9294d6901c72387875d68249f4fd397af8
omarhatem97 <omarhatem221@gmail.com>
2021-01-17 03:34:36 +0200

    implement tn in confusion matrix

--- Evaluation.py
+++ Evaluation.py
@@ -1,19 +1,49 @@
 #confusion matrix, accuracy , precision, recall, F1 score
 
+
+
+def calc_tn_forClass(class_no, classes):
+    """calc tn for certain class number"""
+    tn =0
+
+    for i in range(len(classes)):
+        if i == class_no:
+            continue
+        for j in range(len(classes[i])):
+            if(j == class_no):
+                continue
+            tn += classes[i][j]
+
+    return tn
+
+
+
+
 def confusion_matrix (label, predicted_value, num_class):
     """ takes label, predicted_value as vectors
         returns confusion matrix , tp, fp, tn, fn """
     classes = list(range(num_class))
 
+    tp,fp,tn,fn = 0, 0, 0, 0
 
     #initialize key,values  2d array
     for i in classes:
         classes[i] = [0] * num_class
 
+    #Build confusion matrix
     for i in range(len(label)):
         classes[label[i]][predicted_value[i]] += 1
 
-    return classes
+    #compute tp
+    for i in range(len(classes)):
+        tp += classes[i][i]
+
+    #compute tn
+    for i in range(len(classes)):
+        tn += calc_tn_forClass(i, classes)
+
+
+    return classes,tp,tn
 
     
 
@@ -68,8 +98,10 @@ def print_2dlist(arr):
 
 if __name__ == '__main__':
 
-    label =           [0,1,2,3,4,5,1]
-    predicted_value = [0,1,2,4,5,3,1]
+    label =           [0,1,2,1,0]
+    predicted_value = [0,1,2,1,2]
 
-    out = confusion_matrix(label, predicted_value, 6)
+    out,tp,tn = confusion_matrix(label, predicted_value, 3)
     print_2dlist(out)
+    print(tp)
+    print(tn)
