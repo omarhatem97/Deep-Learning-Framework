e3ad2a0f4ff3d9c2860ae04c444c77136bacc109
omarhatem97 <omarhatem221@gmail.com>
2021-01-17 03:03:20 +0200

    implement confusion matrix intially

--- Evaluation.py
+++ Evaluation.py
@@ -1,9 +1,21 @@
 #confusion matrix, accuracy , precision, recall, F1 score
 
-def confusion_matrix (label, predicted_value):
+def confusion_matrix (label, predicted_value, num_class):
     """ takes label, predicted_value as vectors
         returns confusion matrix , tp, fp, tn, fn """
+    classes = list(range(num_class))
 
+
+    #initialize key,values  2d array
+    for i in classes:
+        classes[i] = [0] * num_class
+
+    for i in range(len(label)):
+        classes[label[i]][predicted_value[i]] += 1
+
+    return classes
+
+    
 
 
 def accuracy (label, predicted_value):
@@ -41,3 +53,23 @@ def F1_score(label, predicted_value):
     Recall = recall(label, predicted_value)
     F1 = 2 * (Precision * Recall)/(Precision + Recall)
     return F1
+
+
+def print_2dlist(arr):
+
+    idx = 0
+    l = list(range(len(arr)))
+
+    print(str('    ') + str(l))
+    for i in arr:
+        print(str(idx) +'-->' + str(i))
+        idx +=1
+
+
+if __name__ == '__main__':
+
+    label =           [0,1,2,3,4,5,1]
+    predicted_value = [0,1,2,4,5,3,1]
+
+    out = confusion_matrix(label, predicted_value, 6)
+    print_2dlist(out)
