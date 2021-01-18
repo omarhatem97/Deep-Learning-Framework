336047876162d9276aef67848d06fddf897efe1d
Aya Mosaad Mohamed <36517014+AyaMosaad1@users.noreply.github.com>
2021-01-17 14:06:55 -1000

    Definition of loss module

--- loss.py
+++ loss.py
@@ -1,3 +1,22 @@
-def calc_loss(label, prediction, name):
+class loss:
     """takes the correct label , predicted value calculated from the model , name of loss function
-        returns delta , loss"""
+        returns delta , loss
+      
+        1-Forward function 
+        error of X with respect to Y_labels.
+        Args:
+            X: numpy.ndarray of shape (n_batch, n_dim) which (WX).
+            Y: numpy.ndarray of shape (n_batch, n_dim) which (Y_labels).
+        Returns:
+            loss: numpy.float. Mean square error of x.
+
+
+        2- Local gradient function
+        Local gradient with respect to X at (X, Y).
+        Args:
+            X: numpy.ndarray of shape (n_batch, n_dim) which (WX).
+            Y: numpy.ndarray of shape (n_batch, n_dim) which (Y_labels).
+        Returns:
+            gradX: numpy.ndarray of shape (n_batch, n_dim) which delta .
+        """
+pass 
