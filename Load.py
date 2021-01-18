7f860c5ca198434862d7f53a09f14ea06453cce6
Maisuody <50196370+Maisuody@users.noreply.github.com>
2021-01-18 20:40:27 +0200

    Update Load.py
    "load data to return matricss"

--- Load.py
+++ Load.py
@@ -1,5 +1,29 @@
-def load (img, X):
-    """load image into matrix X
-        return matrix X"""
+import csv
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
-    return None
+def loadData():
+  datasets = pd.read_csv("train.csv")
+
+  #datasets.head() #show first 4 rows
+  #datasets.describe() #Return all the statics for every row x col
+
+  #visyalize the data
+  y = datasets['label'].iloc[0:10].values
+  x = datasets['pixel10'].iloc[0:10].values
+  #print(x)
+  fig = plt.figure()
+  ax = plt.subplot(111)
+
+  ax.plot(x,y,lw=4)
+  ax.set_xlabel('pixels',fontsize=14)
+  ax.set_ylabel('Its Label',fontsize=14)
+  ax.set_title('VisualizeMyData',fontsize=14)
+
+  #plt.show()
+
+  all_pixels = np.array(datasets.iloc[:,1:])
+  Labels = np.array(datasets['label'])
+
+  return all_pixels, Labels
