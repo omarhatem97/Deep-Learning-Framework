190114ba304f6309bf6f8603350146505c6e6c1d
Aya Mosaad Mohamed <36517014+AyaMosaad1@users.noreply.github.com>
2021-01-18 09:46:04 -1000

    function to open folder

--- Load.py
+++ Load.py
@@ -1,5 +1,16 @@
-def load (img, X):
-    """load image into matrix X
-        return matrix X"""
+import os
+import numpy as np
 
-    return None
+"""
+1- path_of_file 
+load image from folder 
+Args:
+    path_folder : path of folder of dataset
+Returns:
+    array of paths of files 
+"""
+def path_of_file (path_folder):
+    folder = path_folder
+    onlyfiles = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+
+    return onlyfiles
