fixing softmax forward

activations.py
@@ -71,7 +71,7 @@ def softmax(x):
     Returns:
     return -- activation of x using softmax
     """
-    e_x = np.exp(x - np.max(x))
+    e_x = np.exp(x - np.max(x,axis=1, keepdims=True))
     return e_x / np.sum(e_x,axis=1, keepdims=True)
        
 
@@ -90,9 +90,9 @@ def softmax_grad(x):
 
 
 
-x = np.random.rand(100,10) * 5
+# x = np.random.rand(100,10) * 5
 
-z = softmax(x)
-z = softmax_grad(z)
-print(z.shape)
-print(np.sum(z[0]))
+# z = softmax(x)
+# z = softmax_grad(z)
+# print(z.shape)
+# print(np.sum(z[0]))